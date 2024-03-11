45e77d18de2e14d9c687b3e397e5e3a3d9401836
keita1899 <saboten18k@gmail.com>
2024-03-11 18:42:04 +0900

    Todoコンポーネントの中の編集フォームをAppコンポーネントで表示するように修正

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,28 +1,19 @@
 import React from 'react'
 import { TodoProps } from '../types/todo'
-import { EditTodoForm } from './EditTodoForm'
 
 type Props = {
   todo: TodoProps,
   onCompleteTodo: () => void,
   onToggleEditTodo: () => void,
-  onSaveTodo: (updateTodoText: string) => void 
 }
 
-export const Todo:React.VFC<Props> = ({todo, onCompleteTodo, onToggleEditTodo, onSaveTodo}) => {
+export const Todo:React.VFC<Props> = ({todo, onCompleteTodo, onToggleEditTodo}) => {
 
   return (
-    <div>
-      {!todo.isEdit && 
-        <div className="todo">
-          <button className="complete-button" onClick={onCompleteTodo}>完了</button>
-          <span className="todo-text">{todo.text}</span>
-          <button className="edit-button" onClick={onToggleEditTodo}>編集</button>
-        </div>
-      }
-      {todo.isEdit && 
-        <EditTodoForm todoText={todo.text} onToggleEditTodo={onToggleEditTodo} onSaveTodo={onSaveTodo} />
-      }
+    <div className="todo">
+      <button className="complete-button" onClick={onCompleteTodo}>完了</button>
+      <span className="todo-text">{todo.text}</span>
+      <button className="edit-button" onClick={onToggleEditTodo}>編集</button>
     </div>
   )
 }
